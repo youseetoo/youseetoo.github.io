abe4b4a242fd33f8d2b1cc48f19436040350127f
beniroquai <bene.d@gmx.de>
2023-04-24 23:12:00 +0200

    Adding Algoloia search index

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,6 +71,36 @@ const config = {
           },
         ],
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'DMM0XHVBNE',
+  
+        // Public API key: it is safe to commit it
+        apiKey: '189458dc6e096d90e28c263a2a88e9c2',
+  
+        indexName: 'matchboxscopeio',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        //externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        /*replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
+        },
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+        */
+  
+        //... other Algolia params
+      },
       footer: {
         style: 'dark',
         links: [
