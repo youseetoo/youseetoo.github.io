65bb6a36e8668db5faebebfa732b51b864296478
youseetoo <bene.d@gmx.de>
2026-05-22 07:27:16 +0000

    Deploy OptiKit configurator build from 0ea78f939ce3169b3bb052a444e8b766d0d42d3a

--- configurator/assets/jszip.min-CvW_LN9L.js
+++ configurator/assets/jszip.min-CvW_LN9L.js
@@ -1,4 +1,4 @@
-import{a1 as bt,n as It}from"./index-CyILPAoY.js";function vt(yt){throw new Error('Could not dynamically require "'+yt+'". Please configure the dynamicRequireTargets or/and ignoreDynamicRequires option of @rollup/plugin-commonjs appropriately for this require call to work.')}var kt={exports:{}};/*!
+import{K as bt,g as It}from"./index-BO6VGUJh.js";function vt(yt){throw new Error('Could not dynamically require "'+yt+'". Please configure the dynamicRequireTargets or/and ignoreDynamicRequires option of @rollup/plugin-commonjs appropriately for this require call to work.')}var kt={exports:{}};/*!
 
 JSZip v3.10.1 - A JavaScript class for generating and reading zip files
 <http://stuartk.com/jszip>
